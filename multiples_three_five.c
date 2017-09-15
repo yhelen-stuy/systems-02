cd0e0819b5ee66c4fe07c72a1aacb5266e56cc33
Helen Ye <00yhelen@gmail.com>
2017-09-15 19:59:00 -0400

    Solve problem 1 multiples of 3 and 5

---
+++ multiples_three_five.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int i;
+    int sum = 0;
+    for(i = 0; i * 3 < 1000; i++) {
+        if(i * 3 % 5 != 0) {
+            sum += i * 3;
+        }
+    }
+
+    for(i = 0; i * 5 < 1000; i++) {
+        sum += i * 5;
+    }
+
+    printf("sum: %d\n", sum);
+    return 0;
+}
