bcb52b0617d4049bdc4a69c5b5efac42a45f0b09
Helen Ye <00yhelen@gmail.com>
2017-09-15 20:01:36 -0400

    Add comments

--- multiples_three_five.c
+++ multiples_three_five.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/*
+ * Problem:
+ * Find the sum of all the multiples of 3 or 5 below 1000
+*/
+
 int main() {
     int i;
     int sum = 0;
     for(i = 0; i * 3 < 1000; i++) {
+        // Exclude multiples of five so they aren't added twice
         if(i * 3 % 5 != 0) {
             sum += i * 3;
         }
